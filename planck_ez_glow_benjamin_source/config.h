70977fec8b7251501b76fff6e8ec6586554d70a3
lewibs <benjaminsl2000@gmail.com>
2022-08-16 14:44:03 -0400

    added timeout

---
+++ planck_ez_glow_benjamin_source/config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+#define STARTUP_SONG SONG(PLANCK_SOUND)
+#endif
+
+#define MIDI_BASIC
+
+#define ENCODER_RESOLUTION 4
+
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define RGB_MATRIX_STARTUP_SPD 60
