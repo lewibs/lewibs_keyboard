93a3abde0da362b4f56a23a0d370f28eb46e77c8
lewibs <benjaminsl2000@gmail.com>
2024-07-29 11:17:11 -0400

    added some updates

---
+++ planck_ez_glow_benjamin-fork_source/config.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#ifdef AUDIO_ENABLE
+#define STARTUP_SONG SONG(PLANCK_SOUND)
+#endif
+
+#define MIDI_BASIC
+
+#define ENCODER_RESOLUTION 4
+
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+
+#define ORYX_CONFIGURATOR
+#undef DEBOUNCE
+#define DEBOUNCE 10
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#undef MOUSEKEY_TIME_TO_MAX
+#define MOUSEKEY_TIME_TO_MAX 22
+
+#define FIRMWARE_VERSION u8"aPpXb/a6vn5"
+#define RAW_USAGE_PAGE 0xFF60
+#define RAW_USAGE_ID 0x61
+#define LAYER_STATE_8BIT
+
+#define RGB_MATRIX_STARTUP_SPD 60
+
