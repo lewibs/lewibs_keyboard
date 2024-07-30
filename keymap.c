f616176eb03847c861832c9295e50b0445d8804c
lewibs <benjaminsl2000@gmail.com>
2024-07-30 17:17:52 -0400

    removed music

--- keymap.c
+++ keymap.c
@@ -267,78 +267,78 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
-#ifdef AUDIO_ENABLE
-bool muse_mode = false;
-uint8_t last_muse_note = 0;
-uint16_t muse_counter = 0;
-uint8_t muse_offset = 70;
-uint16_t muse_tempo = 50;
+// #ifdef AUDIO_ENABLE
+// bool muse_mode = false;
+// uint8_t last_muse_note = 0;
+// uint16_t muse_counter = 0;
+// uint8_t muse_offset = 70;
+// uint16_t muse_tempo = 50;
 
-void encoder_update(bool clockwise) {
-    if (muse_mode) {
-        if (IS_LAYER_ON(_RAISE)) {
-            if (clockwise) {
-                muse_offset++;
-            } else {
-                muse_offset--;
-            }
-        } else {
-            if (clockwise) {
-                muse_tempo+=1;
-            } else {
-                muse_tempo-=1;
-            }
-        }
-    } else {
-        if (clockwise) {
-        #ifdef MOUSEKEY_ENABLE
-            register_code(KC_MS_WH_DOWN);
-            unregister_code(KC_MS_WH_DOWN);
-        #else
-            register_code(KC_PGDN);
-            unregister_code(KC_PGDN);
-        #endif
-        } else {
-        #ifdef MOUSEKEY_ENABLE
-            register_code(KC_MS_WH_UP);
-            unregister_code(KC_MS_WH_UP);
-        #else
-            register_code(KC_PGUP);
-            unregister_code(KC_PGUP);
-        #endif
-        }
-    }
-}
+// void encoder_update(bool clockwise) {
+//     if (muse_mode) {
+//         if (IS_LAYER_ON(_RAISE)) {
+//             if (clockwise) {
+//                 muse_offset++;
+//             } else {
+//                 muse_offset--;
+//             }
+//         } else {
+//             if (clockwise) {
+//                 muse_tempo+=1;
+//             } else {
+//                 muse_tempo-=1;
+//             }
+//         }
+//     } else {
+//         if (clockwise) {
+//         #ifdef MOUSEKEY_ENABLE
+//             register_code(KC_MS_WH_DOWN);
+//             unregister_code(KC_MS_WH_DOWN);
+//         #else
+//             register_code(KC_PGDN);
+//             unregister_code(KC_PGDN);
+//         #endif
+//         } else {
+//         #ifdef MOUSEKEY_ENABLE
+//             register_code(KC_MS_WH_UP);
+//             unregister_code(KC_MS_WH_UP);
+//         #else
+//             register_code(KC_PGUP);
+//             unregister_code(KC_PGUP);
+//         #endif
+//         }
+//     }
+// }
 
-void matrix_scan_user(void) {
-#ifdef AUDIO_ENABLE
-    if (muse_mode) {
-        if (muse_counter == 0) {
-            uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
-            if (muse_note != last_muse_note) {
-                stop_note(compute_freq_for_midi_note(last_muse_note));
-                play_note(compute_freq_for_midi_note(muse_note), 0xF);
-                last_muse_note = muse_note;
-            }
-        }
-        muse_counter = (muse_counter + 1) % muse_tempo;
-    }
-#endif
-}
+// void matrix_scan_user(void) {
+// #ifdef AUDIO_ENABLE
+//     if (muse_mode) {
+//         if (muse_counter == 0) {
+//             uint8_t muse_note = muse_offset + SCALE[muse_clock_pulse()];
+//             if (muse_note != last_muse_note) {
+//                 stop_note(compute_freq_for_midi_note(last_muse_note));
+//                 play_note(compute_freq_for_midi_note(muse_note), 0xF);
+//                 last_muse_note = muse_note;
+//             }
+//         }
+//         muse_counter = (muse_counter + 1) % muse_tempo;
+//     }
+// #endif
+// }
 
-bool music_mask_user(uint16_t keycode) {
-    switch (keycode) {
-    case RAISE:
-    case LOWER:
-        return false;
-    default:
-        return true;
-    }
-}
-#endif
+// bool music_mask_user(uint16_t keycode) {
+//     switch (keycode) {
+//     case RAISE:
+//     case LOWER:
+//         return false;
+//     default:
+//         return true;
+//     }
+// }
+// #endif
 
 uint8_t layer_state_set_user(uint8_t state) {
-    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
 }
 
 
