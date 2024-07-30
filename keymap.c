5f9a5200ed15e4b317a5ee48878ae6fd548e9407
lewibs <benjaminsl2000@gmail.com>
2024-07-30 17:29:38 -0400

    testing removal of switch case

--- keymap.c
+++ keymap.c
@@ -137,33 +137,12 @@ void set_layer_color(int layer) {
 
 bool rgb_matrix_indicators_user(void) {
   if (keyboard_config.disable_layer_led) { return false; }
-  switch (biton32(layer_state)) {
-    case 0:
-      set_layer_color(0);
-      break;
-    case 1:
-      set_layer_color(1);
-      break;
-    case 2:
-      set_layer_color(2);
-      break;
-    case 3:
-      set_layer_color(3);
-      break;
-    case 4:
-      set_layer_color(4);
-      break;
-    case 5:
-      set_layer_color(5);
-      break;
-    case 6:
-      set_layer_color(6);
-      break;
-   default:
-    if (rgb_matrix_get_flags() == LED_FLAG_NONE)
-      rgb_matrix_set_color_all(0, 0, 0);
-    break;
-  }
+  
+  set_layer_color(layer_state);
+  
+  if (rgb_matrix_get_flags() == LED_FLAG_NONE)
+    rgb_matrix_set_color_all(0, 0, 0);
+
   return true;
 }
 
